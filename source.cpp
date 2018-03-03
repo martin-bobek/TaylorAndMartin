6c12e59bb2076217477ca80b392868b06120b388
Martin Bobek <martinbobek77@hotmail.com>
2018-03-03 21:33:25 +0000

    Changed name of upper to ToUpper.

--- source.cpp
+++ source.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
 int main(int argc, char *argv[]);
-void upper(char str[]);
+void toUpper(char str[]);
 
 int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; i++) {
-        upper(argv[i]);
+        toUpper(argv[i]);
         std::cout << argv[i] << std::endl;
     }
 }
 
-void upper(char str[]) {
+void toUpper(char str[]) {
     for(int i = 0; str[i] != '\0' ; i++)
 	{
 		if( str[i] >= 'a' && str[i] <= 'z')
