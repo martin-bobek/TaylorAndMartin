52c1333817f89634afd99e8655f0f24e0e660775
Martin Bobek <martinbobek77@hotmail.com>
2018-03-03 21:16:31 +0000

    Wrote main function.

--- source.cpp
+++ source.cpp
@@ -1,9 +1,13 @@
+#include <iostream>
 
 int main(int argc, char *argv[]);
 void upper(char str[]);
 
 int main(int argc, char *argv[]) {
-    
+    for (int i = 1; i < argc; i++) {
+        upper(argv[i]);
+        std::cout << argv[i] << std::endl;
+    }
 }
 
 void upper(char str[]) {
