42676859a060dc0634330b3728b489f464d0d11c
Taylor Wong <tay662211@hotmail.com>
2018-03-03 14:07:25 -0700

    I did the upper function.

--- source.cpp
+++ source.cpp
@@ -7,5 +7,12 @@ int main(int argc, char *argv[]) {
 }
 
 void upper(char str[]) {
-    
+    for(int i = 0; str[i] != '\0' ; i++)
+	{
+		if( str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] += 'A' - 'a';
+		
+		}
+	}
 }
